9d0f094511431d27c38919125ddb4a3c7783067e
MarioPasc <mario.pg02@gmail.com>
2025-11-20 20:42:35 +0100

    Remove wrongly imported distdiff subtree

--- medsyn/models/distdiff/medsyn/models/bVAE/__init__.py
+++
@@ -1,7 +0,0 @@
-# medsyn/models/bVAE/__init__.py
-"""Conditional β-VAE model for PathMNIST synthesis."""
-
-from .model import ConditionalBetaVAE
-from .config import load_bvae_config, BVAEConfig
-
-__all__ = ["ConditionalBetaVAE", "load_bvae_config", "BVAEConfig"]
